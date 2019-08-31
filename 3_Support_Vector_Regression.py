bb693cb9eac94132d82eb15d7213edf7192abae3
tjddbs0401 <48537546+tjddbs0401@users.noreply.github.com>
2019-08-31 11:39:31 +0900

    Update and rename Support_Vector_Regression.py to 3_Support_Vector_Regression.py

--- 3_Support_Vector_Regression.py
+++ 3_Support_Vector_Regression.py
@@ -3,4 +3,4 @@ from sklearn.svm import SVR
 svr = SVR(kernel='rbf')
 svr.fit(X_train, y_train)
 
-print(mean_squared_error(svr.predict(X_test), y_test))
+print(mean_squared_error(svr.predict(X_test), y_test))#가장 오차값이 
